e86d94524b65b5df279c6d14cf2d694be364c67f
uhidontkno <hi@rare1k.dev>
2024-06-29 23:14:02 -0400

    feat: init tailwindcss, daisyui, and catppuccin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
+
+import cat from "@catppuccin/daisyui"
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,css}"],
+  content: ["./public/**/*.{html,css}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "night",
+      cat("mocha","mauve"),
+      cat("macchiato","mauve"),
+      cat("frappe","mauve"),
+      cat("latte","mauve"),
+      "light"
+    ]
+  }
 }
 
