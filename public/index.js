eef05d3200ac859c6f02991cecf9e77fcf1ba369
uhidontkno <hi@rare1k.dev>
2024-06-30 09:49:29 -0400

    feat: finally get uv working somehow

--- public/index.js
+++ public/index.js
@@ -19,4 +19,18 @@ form.addEventListener("submit", async (event) => {
   let frame = document.getElementById("frame");
   frame.style.display = "block";
   frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+  removeShit()
 });
+
+
+function removeShit() {
+  let si = document.querySelector(".searchInput").cloneNode(true)
+  document.querySelector(".contain").remove()
+  document.querySelector("footer").remove()
+  document.querySelector("nav").appendChild(si)
+  si.querySelector(".btn").addEventListener("click",()=>{
+    const url = search(address.value, searchEngine.value);
+    let frame = document.getElementById("frame");
+    frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+  })
+}
